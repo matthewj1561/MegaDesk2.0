f3dba8c71a148222ff3edf2cfa62261f3baaefb9
Matthew James <matthewj1561@gmail.com>
2022-10-12 14:37:08 -0600

    Quotes are stored in json file. Rush is pulled from txt file

--- MegaDesk2.0/DeskQuote.cs
+++ MegaDesk2.0/DeskQuote.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -32,11 +34,69 @@ namespace MegaDesk_James
             
         }
 
-        int[,] rushPrices = new int[,] { {0,0,0 },{ 60, 70, 80 }, { 40, 50, 60 }, { 30, 35, 40 } };
+        //int[,] rushPrices = new int[,] { {0,0,0 },{ 60, 70, 80 }, { 40, 50, 60 }, { 30, 35, 40 } };
         int[] surfaceCosts = new int[] {200, 100, 50, 300, 125 };
+        private int[,] GetRushOrder ()
+        {
+            int[,] finalPrices = new int[4, 3];
+            List<int> values = new List<int>();
+            string path = @"C:\Users\matth\source\repos\MegaDesk2.0\MegaDesk2.0\cit365_document_rushOrderPrices.txt";
+            string[] readText = File.ReadAllLines(path);
+            foreach(string price in readText)
+            {
+                values.Add(int.Parse(price));
+            }
+            int counter = 0;
+            for(int i = 0; i < 4; i++)
+            {
+                if (i == 0)
+                {
+                    for (int j = 0; j < 3; j++)
+                    {
+                        finalPrices[i, j] = 0;
+                    }
+                }
+                else
+                {
+                    for (int j = 0; j < 3; j++)
+                    {
+                        finalPrices[i, j] = values[counter];
+                        counter += 1;
+                    }
+                }
+            }
+            return finalPrices;
+        }
+
+        public static void Serialize(object obj, string filePath)
+        {
+            var serializer = new JsonSerializer();
+
+            using (var sw = new StreamWriter(filePath))
+            using (JsonWriter writer = new JsonTextWriter(sw))
+            {
+                serializer.Serialize(writer, obj);
+            }
+        }
+
+        public static object Deserialize(string path)
+        {
+            var serializer = new JsonSerializer();
+
+            using (var sw = new StreamReader(path))
+            using (var reader = new JsonTextReader(sw))
+            {
+                return serializer.Deserialize(reader);
+            }
+        }
 
         public void getQuotePrice()
         {
+            int[,] rushPrices = GetRushOrder();
+            foreach(int price in rushPrices)
+            {
+                Debug.WriteLine(price);
+            }
             decimal basePrice = 200;
             decimal surfaceAreaPrice = 0;
             decimal drawCost = 0;
@@ -67,6 +127,8 @@ namespace MegaDesk_James
             decimal totalCost = basePrice + drawCost + rushCost + surfaceAreaPrice + surfaceCosts[(int)d.DesktopMaterial];
 
             quotePrice = totalCost;
+            Serialize(this, @"C:\\Users\\matth\\source\\repos\\MegaDesk2.0\\MegaDesk2.0\\quotes.json");
+
 
 
 
